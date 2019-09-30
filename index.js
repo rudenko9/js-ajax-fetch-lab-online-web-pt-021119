634a7f667387595ef4c30b3efad51a68925e43fe
github username <github email address>
2019-09-30 15:39:22 -0400

    Done.

--- index.js
+++ index.js
@@ -15,17 +15,30 @@ function forkRepo() {
     headers: {
       Authorization: `token ${getToken()}`}
   }).then(res => res.json()).then(json => showForkedRepo(json))};
-  
+
 
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+  fetch(`https://api.github.com/repos/rudenko9/js-ajax-fetch-lab/issues`, {
+    method: 'POST',
+    headers: {Authorization: `token ${getToken()}`},
+    body: JSON.stringify(postData)
+  }).then(res => res.json()).then(json => getIssues());
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch(`https://api.github.com/repos/rudenko9/js-ajax-fetch-lab/issues`, {
+    headers: {Authorization: `token ${getToken()}`},
+  }).then(res => res.json()).then(json => showIssue(jason))
 }
